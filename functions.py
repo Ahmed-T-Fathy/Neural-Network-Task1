d3ae1a6258b015efde1f2ff16f745a18dbd32f66
Ahmed Nouh <104303369+AhmedSaidNouh@users.noreply.github.com>
2022-11-01 22:01:58 +0200

    function updated

--- functions.py
+++ functions.py
@@ -1,12 +1,28 @@
-def gradient_descent(epochs ,rate):
+import numpy as np
+import random
+
+def gradient_descent(epochs, rate, x_train, y_train, f1, f2):
+    n = float(len(x_train))
+    w1 = random.random()
+    w2 = random.random()
     for i in range(epochs):
-        Y_pred = m * X + c  # The current predicted value of Y
-        D_m = (-2 / n) * sum((Y - Y_pred) * X)  # Derivative wrt m
-        D_c = (-2 / n) * sum(Y - Y_pred)  # Derivative wrt c
-        m = m - L * D_m  # Update m
-        c = c - L * D_c  # Update c
+        predct_matrix = w1 * x_train[f1] + w2 * x_train[f2]
+        y_pred = signum(predct_matrix)  # The current predicted value of Y
+        D_w1 = (-2/n) *sum((y_train - y_pred) * x_train[f1])  # Derivative wrt w1
+        D_w2 =(-2/n) * sum((y_train - y_pred) * x_train[f2])  # Derivative wrt w2
+        w1 = w1 - rate * D_w1  # Update w1
+        w2 = w2 - rate * D_w2  # Update w2
+    return w1,w2
+
+
 def signum(input):
-    if(input>=0):
-        return 1
-    else:
-        return 0
+    input=np.array(input)
+    n = len(input)
+    for i in range(n):
+        if (input[i] >= 0):
+           input[i] = 1
+        else:
+            input[i] = -1
+
+
+    return input
